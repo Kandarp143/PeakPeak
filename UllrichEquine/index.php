c4d44b747b75965c465b3353a4b1ede3880612c0
Kandarp <Kandarp@eduroam-ipv4-1336.triple-a.uni-kl.de>
2016-09-02 22:23:07 +0200

    Add Subpages

---
+++ UllrichEquine/index.php
@@ -0,0 +1,447 @@
+
+		
+		
+<?
+	ini_set('display_errors', 1); 
+	error_reporting(E_ALL);
+	require_once('functions.php');
+?>
+<!doctype html>
+<html>
+	<head>
+		<script src="//cdn.optimizely.com/js/3951220563.js"></script>
+		<title>Equine Sales Manager werden</title>
+		<meta charset="utf-8" />
+		<link rel="icon" href="img/favicon.png">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+		<!-- bootstrap -->
+		<script src="https://code.jquery.com/jquery-2.1.4.min.js" ></script>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		
+		<!-- fonts -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+
+		<!-- stylesheets -->
+		<link rel="stylesheet" type="text/CSS" href="_resources/lib/nouislider/nouislider.min.css">
+		<link rel="stylesheet" type="text/CSS" href="style-b.css" />
+		<link rel="stylesheet" type="text/CSS" href="slider.css">	
+		
+		<!-- scripts -->
+		<script src="_resources/lib/polyfiller/polyfiller.js"></script>
+		<script src="_resources/lib/nouislider/nouislider.min.js"></script>
+		<script src="js/main.js"></script>
+		<script src="js/slider.js"></script>
+		
+		
+		<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+			
+		
+		
+		<script>
+		//webshim.setOptions('basePath', '/js-webshim/minified/shims/');
+		
+		//request the features you need:
+		webshim.polyfill('es5 mediaelement forms');
+		
+		$(function(){
+		// use all implemented API-features on DOM-ready
+		});
+		</script>
+	
+		<script type="text/javascript">
+		  var _mfq = _mfq || [];
+		  (function() {
+		    var mf = document.createElement("script");
+		    mf.type = "text/javascript"; mf.async = true;
+		    mf.src = "//cdn.mouseflow.com/projects/d060cc37-1e14-48b1-baa3-250febd20154.js";
+		    document.getElementsByTagName("head")[0].appendChild(mf);
+		  })();
+		</script>
+		<!-- Facebook Conversion Code for Leads - UEI 1 -->
+		
+		
+		
+		<!-- Facebook Pixel Code -->
+		<script>
+		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+		n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+		document,'script','//connect.facebook.net/en_US/fbevents.js');
+		
+		fbq('init', '1471188376544111');
+		fbq('track', "PageView");</script>
+		<noscript><img height="1" width="1" style="display:none"
+		src="https://www.facebook.com/tr?id=1471188376544111&ev=PageView&noscript=1"
+		/></noscript>
+		<!-- End Facebook Pixel Code -->
+		
+		
+		<!-- Hotjar Tracking Code for www.ullrichequine.de -->
+		<script>
+		    (function(h,o,t,j,a,r){
+		        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+		        h._hjSettings={hjid:116510,hjsv:5};
+		        a=o.getElementsByTagName('head')[0];
+		        r=o.createElement('script');r.async=1;
+		        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+		        a.appendChild(r);
+		    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+		</script>
+		
+		
+
+
+	</head>
+	<body>
+	
+	
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-70313552-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+		
+		
+		<header>
+		
+			<nav>
+			
+				<div class="row inner-wrapper">
+					 
+					<div class="col-md-6 col-sm-3 col-xs-12" id="logo">
+						<img src="img/logo.png" />
+					</div>
+					
+					<div class="col-md-2 col-sm-5 hidden-xs" id="slogan">
+						<div class="slogan"></div>
+					</div>
+					
+					<div class="col-md-4 col-sm-4 col-xs-4 hidden-xs pull-right text-right" id="phone">
+						<i class="fa fa-phone-square"></i>
+						<span class="value">06221 355 99 88</span>
+					</div>
+					
+				</div>
+				
+			</nav>
+			
+			<div class="line1"></div>
+			<div class="line2"></div>
+			
+		</header>
+		
+		<div id="page">
+			
+			<div id="teaser" style="position:relative; background-image:url('img/teaser-gradient.jpg') !important;" class="black">
+				
+				<div class="form-wrapper">
+						
+						<div class="overlay">
+							<h2>Geld verdienen im Reitsport?<br> Mit UllrichEquine kein Problem!</h2>
+		
+							<div class="row">
+								<div class="col-md-12">
+									<ul class="fa-ul benefits">
+										<li><i class="fa fa-li fa-lg fa-check-square"></i>Grenzenlose Möglichkeiten ohne Risiko</li>
+										<li><i class="fa fa-li fa-lg fa-check-square"></i>Unabhängigkeit durch selbständiges Arbeiten</li>
+										<li><i class="fa fa-li fa-lg fa-check-square"></i>Finanzielle Freiheit, Sie gehen Ihren Weg</li>
+										<li><i class="fa fa-li fa-lg fa-check-square"></i>BMW-Autoprogramm für Sie mit UEI-Bonus</li>
+										<li><i class="fa fa-li fa-lg fa-check-square"></i>Innovative Produkte mit Alleinstellungsmerkmal auf Noni-Basis</li>
+										<li><i class="fa fa-li fa-lg fa-check-square"></i>Eigene Pferde im internationalen Sport zeigen den Erfolg der Produkte</li>
+	
+									</ul>
+								</div>
+							</div>
+						</div>
+					
+				</div>
+					
+					
+				<div class="form-container" id="conversion">
+				
+					<div class="row light">
+						<div class="col-md-12">
+						
+							<div class="col-md-12">
+								<h3>Sie möchten mehr erfahren?</h3>
+							</div>
+							<div class="col-md-12">
+								Hinterlassen Sie Ihre Kontaktdaten und erzählen uns gegebenenfalls über 
+								Ihre Erfahrung mit Pferden.<br><br>
+							</div>
+							
+							<form action="" method="get">
+								<!--<div class="overlay">
+									<div class="message">Vielen Dank für Deine Bewerbung.</div>
+								</div>-->
+									<div class="col-md-12">
+										<input required type="text" name="contact-firstname" placeholder="Vorname">
+									</div>
+									<div class="col-md-12">
+										<input required type="text" name="contact-lastname" placeholder="Nachname">
+									</div>
+									<div class="col-md-12">
+										<input required type="tel" name="contact-phone" placeholder="Telefonnummer">
+									</div>
+									<div class="col-md-12">
+										<input required type="email" name="contact-email" placeholder="E-Mail-Adresse">
+									</div>
+									<div class="col-md-12">
+										<textarea rows="5" name="contact-message" placeholder="Bitte spezifizieren Sie Ihre Pferdeerfahrung"></textarea>
+									</div>
+									<div class="col-md-12 col-xs-12 pull-right text-right">
+										<input type="submit" value="Ja, ich interessiere mich für den Equine Manager." class="btn">
+									</div>
+									<i class="clear showMobile"></i>
+									
+									<div class="col-md-12"><br>Wir werden uns innerhalb der nächsten 48 Stunden  mit Ihnen unverbindlich telefonisch in Verbindung setzen.</div>
+								
+								
+							</form>	
+						</div>
+					</div>
+				</div>
+					
+			</div>
+		
+		
+		
+		
+			<?php /*
+			<div class="sliderWrapper">
+				
+				<div class="inner">
+					<div class="title">Ihr Erfolg im Überblick</div>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet. </p>
+					
+					<div id="salarySlider"></div>
+					<div id="salarySliderSteps">
+						<div class="step"></div>
+						<div class="step"></div>
+						<div class="step"></div>
+						<div class="step"></div>
+						<div class="step"></div>
+						<div class="step"></div>
+						<div class="step"></div>
+					</div>
+					
+					<div id="salarySliderContent">
+						
+						<div class="contentBlock active" id="contentBlock-10">
+							<div class="title">Equine Manager Small</div>
+							<div class="content">Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam;</div>
+							<a class="btn cta" href="">Jetzt bewerben</a>
+						</div>
+						
+						<div class="contentBlock" id="contentBlock-20">
+							<div class="title">Equine Manager Medium</div>
+							<div class="content">Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam;</div>
+							<a class="btn cta" href="">Jetzt bewerben</a>
+						</div>
+						
+						<div class="contentBlock" id="contentBlock-30">
+							<div class="title">Equine Manager Large</div>
+							<div class="content">Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam;</div>
+							<a class="btn cta" href="">Jetzt bewerben</a>
+						</div>
+						<div class="contentBlock" id="contentBlock-40">
+							<div class="title">Equine Manager Extra Large</div>
+							<div class="content">Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam;</div>
+							<a class="btn cta" href="">Jetzt bewerben</a>
+						</div>
+						<div class="contentBlock" id="contentBlock-50">
+							<div class="title">Equine Manager Extra Extra Large</div>
+							<div class="content">Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam;</div>
+							<a class="btn cta" href="">Jetzt bewerben</a>
+						</div>
+						<div class="contentBlock" id="contentBlock-60">
+							<div class="title">Equine Manager Super</div>
+							<div class="content">Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam;</div>
+							<a class="btn cta" href="">Jetzt bewerben</a>
+						</div>
+						<div class="contentBlock" id="contentBlock-80">
+							<div class="title">Equine Manager Extra Super</div>
+							<div class="content">Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam;</div>
+							<a class="btn cta" href="">Jetzt bewerben</a>
+						</div>
+						<div class="contentBlock" id="contentBlock-70">
+							<div class="title">Equine Manager Best</div>
+							<div class="content">Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam;</div>
+							<a class="btn cta" href="">Jetzt bewerben</a>
+						</div>
+					</div>
+				</div>
+			</div>-->
+			<section class="white" style="padding-top:80px !important;">
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<iframe class="autoplay" width="640" height="360" src="https://www.youtube.com/embed/xGq2cRv7bMk?rel=0&amp;controls=0&amp;showinfo=0;" frameborder="0" ></iframe>
+					</div>
+				</div>
+			</section>
+			<section class="grey">
+				<div class="row">
+					<div class="col-md-5 col-sm-5 text-center">
+						<img src="img/building-uei-heidelberg.jpg" alt="building-uei-heidelberg" width="" height="" />
+					</div>
+					<div class="col-md-7 col-md-7">
+						<h2>UllrichEquine international</h2>
+						<p class="notsmall">In Deutschland gibt es mehr als eine Million Reiterinnen und Reiter sowie ihre Pferde. Innovative Produkte und Vermarktungsstrategien mit Mehrwert für unsere Kunden und Partner zeichnen uns aus. Dabei steht die Gesundheit der Tiere und unserer Kunden an erster Stelle.</p>
+						<p class="notsmall">Völlig neu ist der Gedanke und die Aktion des Direktmarketings von UllrichEquine international GmbH, dem ersten Direktvertrieb von Ergänzungsfuttermitteln für Pferde. Bisher ist das einmalig.</p>
+						<p class="notsmall">Profitieren Sie von Ihrer Erfahrung mit Pferden und Menschen umzugehen und machen Sie Karriere als Equine Manager im Reitsport.</p>
+						
+						<h2>Unsere Erfahrung: Chancen nutzen!</h2>
+						<div class="row">
+							<div class="col-md-8" style="padding-left: 0;">
+								<p class="notsmall">Ich bin seit Jahren dem Pferdesport eng verbunden und liebe es hier tätig zu sein, sowie Menschen die Chance zu geben, sich selber eine berufliche Karriere aufzubauen.</p>
+						
+								<p class="notsmall">Wir haben das UEI-Konzept entwickelt und bieten Ihnen die Möglichkeit, sich risikolos ein eigenes Geschäft aufzubauen mit grenzenlosen Verdienstchancen.</p>
+							</div>
+							<div class="col-md-4">
+								<img src="img/juergen-m-ullrich-ceo.png" alt="juergen-m-ullrich-ceo" width="" height="" />
+								<div class="subtitle">
+									<div><strong>Jürgen M. Ullrich</strong></div>
+									<div>CEO & Founder</div>
+								</div>
+							</div>				
+					</div>
+					
+				</div>
+			</section>
+			<section class="white testimonial">
+				<div class="row">
+					<div class="col-md-12">
+						<h2>UllrichEquine Fashion Leader</h2>
+
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-6 block"><img src="img/testimonials/dorothee-schneider-ullrich-equine.jpg" alt="dorothee-schneider-ullrich-equine" width="" height="" />
+						<div class="subtitle">Dorothee Schneider</div>
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-6 block"><img src="img/testimonials/emily-k-paulsen-ullrich-equine.jpg" alt="emily-k-paulsen-ullrich-equine" width="" height="" />
+						<div class="subtitle">Emily K. Paulsen</div>
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-6 block"><img src="img/testimonials/hanne-brenner-ullrich-equine.jpg" alt="david-will-ullrich-equine" width="" height="" />
+						<div class="subtitle">Hanne Brenner</div>
+					</div>
+					<div class="col-md-3 col-sm-3 col-xs-6 block"><img src="img/testimonials/marcel-wegfahrt-ullrich-equine.jpg" alt="marcel-wegfahrt-ullrich-equine" width="" height="" />
+						<div class="subtitle">Marcel Wegfahrt</div>
+					</div>
+				</div>
+			</section>
+			<section class="grey">
+				<div class="row">
+					<div class="col-md-12">
+						<h2>Equine Manager fahren BMW</h2>
+					</div>
+					<div class="col-md-6">
+						<p>Wir bieten Equine Managern zu besonders günstigen Leasingbedingungen mit monatlichem Leasingzuschuss 5 BMW-Modelle zu Sonderkonditionen an.</p>
+					</div>
+					<div class="col-md-6">
+						<img src="img/cars.png" alt="cars" width="" height="" />
+					</div>
+				</div>
+			</section>
+			<section class="white">
+				<div class="row">
+					<div class="col-md-12">
+						<h2>Ihr Erfolg im Überblick</h2>
+					</div>
+					
+					<div class="col-md-7">
+						<p>Sie verkaufen an begeisterte Reitsportfreunde risikolos und ohne Eigenkapital unsere Produkte hoher Qualität und sichern so Ihre Karriere und Ihr Einkommen als UEI Equine Manager.</p>
+					</div>
+					<div class="col-md-5">
+						<img src="img/diagramm-rangfolge-transparent.png" alt="diagramm-rangfolge-transparent" width="" height="" />
+					</div>
+				</div>
+			</section>
+			<section class="grey">
+				<div class="row">
+					<div class="col-md-7 col-sm-7">
+						<h3>Noni für Mensch, Pferd und Hund</h3>
+						<p>Der Noni-Baum (Morinda citrifolia) stammt ursprünglich aus Australien, verbreitete sich aber im ganzen Raum des Indischen Ozeans sowie in Polynesien. Schon vor tausenden von Jahren erkannten die Menschen das besondere Potential der Noni-Frucht und sorgten für deren Verbreitung.</p>
+						<p>Das uralte Wissen der pazifischen Naturvölker über Noni wird noch heute von Generation zu Generation weitergegeben.</p>
+						<p>Sein Konzentrat ist wichtiger Bestandteil unserer Nahrungsergänzungs- und Futterergänzungsmittel. Noni enthält Aminosäuren, Enzyme sowie die 2fach ungesättigte Linolensäure.</p>
+
+						<p>Noni ist die Frucht mit den meisten Iridoiden überhaupt. Das sind sekundäre Pflanzenstoffe für ihre physiologische Wirkung bekannte, natürliche Bestandteile von Pflanzen.</p>
+												
+					</div>
+					<div class="col-md-5 col-sm-5">
+						<img src="img/noni-fruit--products-collection.png">
+					</div>
+				</div>
+			</section>
+			<section class="green cta">
+				<div class="row">
+					<div class="col-md-12">
+						<h3>Haben wir Ihr Interesse geweckt?</h3>
+						
+						<a class="btn white" href="#page" id="cta-button">Equine Sales Manager werden</a>
+						
+	
+					</div>
+				</div>
+			</section>	
+		
+			*/ ?>	
+		</div>
+		
+		<footer>
+			
+			<div class="col-md-9">&copy; 2015 UllrichEquine international GmbH</div>
+			<div class="col-md-3">
+			<a href="http://www.ullrichequine.de/equine-manager-werden/impressum.php">Impressum</a>&nbsp;
+			<a href="http://www.ullrichequine.de/unternehmen/index.html">Unternehmen</a>&nbsp;
+            <a href="http://www.ullrichequine.de/datenschutz/index.html">Datenschutz</a>
+			</div>
+			<!-- <div class="col-md-3"><a href="http://www.ullrichequine.de/unternehmen/index.html">Unternehmen</a></div> -->
+		
+		</footer>
+		
+		<!-- Mouseflow -->
+		
+		<script type="text/javascript">
+		  var _mfq = _mfq || [];
+		  (function() {
+		    var mf = document.createElement("script");
+		    mf.type = "text/javascript"; mf.async = true;
+		    mf.src = "//cdn.mouseflow.com/projects/7e77478b-c8f6-436d-9b11-08e9f2069a91.js";
+		    document.getElementsByTagName("head")[0].appendChild(mf);
+		  })();
+		</script>
+		
+		<!-- END Mouseflow -->
+		
+		<!-- Google Code für ein Remarketing-Tag -->
+<!--------------------------------------------------
+Remarketing-Tags dürfen nicht mit personenbezogenen Daten verknüpft oder auf Seiten platziert werden, die sensiblen Kategorien angehören. Weitere Informationen und Anleitungen zur Einrichtung des Tags erhalten Sie unter: http://google.com/ads/remarketingsetup
+--------------------------------------------------->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 941781569;
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/941781569/?value=0&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
+	</body>
+</html>
